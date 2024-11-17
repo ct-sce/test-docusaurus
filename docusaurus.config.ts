88eae7dcbd3a94a1c38a545f83a7882d20ffe435
CATHERINE Sébastien <sebastien.catherine@cloud-temple.com>
2024-11-17 03:51:18 +0100

    fix baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ct-sce/test-docusaurus/',
+  baseUrl: '/test-docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
